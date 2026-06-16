f70ba49718a5f5ac0e06315c67c08f688ec41a76
felix.wong <felix.wong@altech.hk>
2026-06-16 09:56:48 +0800

    build: enable kotlin jpa plugin for services

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.21")
 	implementation("org.jetbrains.kotlin:kotlin-allopen:2.3.21") // kotlin("plugin.spring")
+	implementation("org.jetbrains.kotlin:kotlin-noarg:2.3.21") // kotlin("plugin.jpa")
 	implementation("org.springframework.boot:spring-boot-gradle-plugin:4.1.0")
 	implementation("io.spring.gradle:dependency-management-plugin:1.1.7")
 }
